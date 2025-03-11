5bf76865b61fcca966783c69fe6c3ac77d418bf7
Pedro Ulrich <heringer.ulrich@gmail.com>
2025-03-11 16:03:59 -0300

    added "edit and update" to blog_post

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,9 @@ Rails.application.routes.draw do
 
   get "/blog_posts/new", to: "blog_posts#new",
       as: :new_blog_post
+  get "blog_posts/:id/edit", to: "blog_posts#edit", as: :edit_blog_post
   get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
-
+  patch "/blog_posts/:id", to: "blog_posts#update", as: :blog_post_update
   post "/blog_posts", to: "blog_posts#create",
        as: :blog_posts
 
