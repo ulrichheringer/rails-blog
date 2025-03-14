991ef7d52ee0c90c359ed474a1eabe3d122737b1
Pedro Ulrich <heringer.ulrich@gmail.com>
2025-03-14 11:54:48 -0300

    Added PublishedAt attribute to BlogPosts

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_03_12_004210) do
+ActiveRecord::Schema[8.0].define(version: 2025_03_14_135416) do
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
