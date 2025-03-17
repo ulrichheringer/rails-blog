7b93b21bbd15bf2eb6728118c459d8c75124e2c7
Pedro Ulrich <heringer.ulrich@gmail.com>
2025-03-17 17:59:38 -0300

    wrote some tests

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -41,14 +41,13 @@ class BlogPostsController < ApplicationController
 
   private
 
-  def blog_post_params
-    params.require(:blog_post).permit(:title, :body, :published_at)
-  end
+    def blog_post_params
+      params.require(:blog_post).permit(:title, :body, :published_at)
+    end
 
-  def set_blog_post
-    @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost
-                                                                  .published.find(params[:id])
-  rescue ActiveRecord::RecordNotFound
-    redirect_to root_path
-  end
+    def set_blog_post
+      @blog_post = user_signed_in? ? BlogPost.find(params[:id]) : BlogPost.published.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path
+    end
 end
--- test/models/blog_post_test.rb
+++ test/models/blog_post_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class BlogPostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "draft? returns true for draft blog post" do
+    assert BlogPost.new(published_at: nil).draft?
+  end
+
+  test "draft? returns false for published blog post" do
+    assert_not BlogPost.new(published_at: 1.year.ago).draft?
+  end
+
+  test "draft? returns false for scheduled blog post" do
+    assert_not BlogPost.new(published_at: 1.year.from_now).draft?
+  end
+
+  test "published? returns true for published blog post" do
+    assert BlogPost.new(published_at: 1.year.ago).published?
+  end
+
+  test "published? returns false for draft blog post" do
+    assert_not BlogPost.new(published_at: nil).published?
+  end
+
+  test "published? returns false for scheduled blog post" do
+    assert_not BlogPost.new(published_at: 1.year.from_now).published?
+  end
+
+  test "scheduled? returns true for scheduled blog post" do
+    assert BlogPost.new(published_at: 1.year.from_now).scheduled?
+  end
+
+  test "scheduled? returns false for draft blog post" do
+    assert_not BlogPost.new(published_at: nil).scheduled?
+  end
+
+  test "scheduled? returns false for published blog post" do
+    assert_not BlogPost.new(published_at: 1.year.ago).scheduled?
+  end
 end
